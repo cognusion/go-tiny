fa991187ad943c81b8b9056ac2eb0c77bca2ca87
Matt Keller <m@cognusion.com>
2015-03-06 16:03:51 -0500

    A cute little tiny url generator/redirector, mainly as a testbed for go-cache-lru

---
+++ tiny.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"io"
+	"fmt"
+	"github.com/cognusion/go-cache-lru"
+	"time"
+	"crypto/rand"
+	"math/big"
+	"math"
+	"hash/crc32"
+	"strings"
+	"net/http"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+)
+
+var VERSION = "go-tiny 1.0.3"
+var C *cache.Cache
+var GLOBALOFFSET uint32
+
+// Simply return the version
+func version(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s", VERSION)
+}
+
+// Grab an item from the cache and redirect to the stored URL
+func fetch(c web.C, w http.ResponseWriter, r *http.Request) {
+	
+	tiny := c.URLParams["tiny"]
+	fmt.Printf("Getting %v\n", tiny)
+	
+	turl, found := C.Get(tiny)
+	if ! found {
+		http.Error(w, http.StatusText(404), 404)
+		return
+	}
+	
+	url := turl.(string)
+	if strings.HasPrefix(url,"http:") || strings.HasPrefix(url,"https:") {
+		url = strings.Replace(url, ":/", "://", 1)	
+	} else {
+		url = "http://" + url
+	}
+	fmt.Printf("Got %v as %v\n", tiny, turl)
+	
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+// Generate the offset crc32, and set it in the cache
+func set(c web.C, w http.ResponseWriter, r *http.Request) {
+	
+	url := r.RequestURI
+	url = url[5:len(url)] // get rid of leading "/set"
+	
+	fmt.Printf("Setting %v\n", url)
+	
+	// We take the crc32 of the URL add a random offset
+	h := crc32.NewIEEE()
+    h.Write([]byte(url))
+    s64 := uint64(h.Sum32()+GLOBALOFFSET)
+    v := fmt.Sprintf("%x", s64)
+	
+	C.Set(v, url, cache.DefaultExpiration)
+	
+	fmt.Printf("Set %v to %v\n", url,v)
+	
+	msg := `<html>
+	<body>
+	<a href="%v">%v</a>
+	</body>
+	</html>
+	`
+	msg = fmt.Sprintf(msg, "/" + v, "/" + v)
+	
+	io.WriteString(w, msg)
+}
+
+
+func main() {
+
+	// So the URLs aren't pure crc32s of the URI
+	thirtyTwo := math.Pow(2,32)-1
+	max := *big.NewInt(int64(thirtyTwo))
+	roff,_ := rand.Int(rand.Reader,&max)
+	GLOBALOFFSET = uint32(roff.Uint64())
+	fmt.Printf("Offset is %v\n",GLOBALOFFSET)
+	
+	// Keep items for 24hours, clean every 30s, cap at 50k items.
+	C = cache.New(24*time.Hour, 30*time.Second, 50000)
+
+	// Set the URI handlers, and go!
+	goji.Get("/set/*", set)
+	goji.Get("/:tiny", fetch)
+	goji.Get("/version", version)
+	goji.Serve()
+
+}
